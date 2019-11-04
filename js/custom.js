e790212f54dbd264455fdaf439c4e0a2078b5035
Nick Mkrtchyan <nickmkrtchyan@gmail.com>
2019-11-04 13:06:42 +0300

    .

--- js/custom.js
+++ js/custom.js
@@ -74,21 +74,7 @@
     });
 
 
-    // Scroll To Animation
-    //-------------------------------------------------------------------------------
-    $('body').scrollspy({target: '#navigation-top-1', offset: 88});
 
-    var scrollTo = $(".scroll-to");
-
-    scrollTo.click(function (event) {
-        $('.modal').modal('hide');
-        var position = $(document).scrollTop();
-        var scrollOffset = 87;
-
-        var marker = $(this).attr('href');
-        $('html, body').animate({scrollTop: $(marker).offset().top - scrollOffset}, 'slow');
-        return false;
-    });
 
 
     // Scroll Up Btn
